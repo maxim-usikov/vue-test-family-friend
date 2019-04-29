22391cb7b91829c68f737c2b7f3d1e58e388f9ec
Maxim Usikov <mu.usikov@gmail.com>
2019-04-29 14:38:19 +0300

    feat(components): add `SearchResults`

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,50 @@
+<template>
+  <ViewPort>
+    <template #default="{ isMobile }">
+      <ResultsEmpty v-if="items.length === 0" />
+      <ResultsTables
+        v-else-if="isMobile"
+        :items="reversedLastThreeItems"
+      />
+      <ResultsTabs
+        v-else
+        :items="reversedLastThreeItems"
+      />
+    </template>
+  </ViewPort>
+</template>
+
+<script>
+import ViewPort from '@/components/ViewPort';
+import ResultsEmpty from '@/components/ResultsEmpty.vue';
+import ResultsTables from '@/components/ResultsTables.vue';
+import ResultsTabs from '@/components/ResultsTabs.vue';
+
+// TODO: tests
+export default {
+  name: 'SearchResults',
+
+  components: {
+    ViewPort,
+    ResultsEmpty,
+    ResultsTables,
+    ResultsTabs,
+  },
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    reversedLastThreeItems() {
+      return this.items.slice(-3).reverse();
+    },
+    lastThreeItems() {
+      return this.items.slice(-3);
+    },
+  },
+};
+</script>
