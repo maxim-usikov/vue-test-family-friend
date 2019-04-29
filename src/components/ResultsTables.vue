70591a2f24ba016c117b53238bdede9ad751caf4
Maxim Usikov <mu.usikov@gmail.com>
2019-04-29 14:37:06 +0300

    feat(components): add `ResultsTables`

---
+++ src/components/ResultsTables.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <template v-for="item in items">
+      <h3 :key="item.id">
+        {{ item.id }}. {{ item.query }}
+      </h3>
+      <RepositoryTable
+        :key="item.id"
+        :items="item.response.data.search.edges"
+      />
+    </template>
+  </section>
+</template>
+
+<script>
+import RepositoryTable from '@/components/RepositoryTable.vue';
+
+// TODO: tests
+export default {
+  name: 'ResultsTables',
+
+  components: {
+    RepositoryTable,
+  },
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
