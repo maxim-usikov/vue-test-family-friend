b5fd26033cdf2cde89404d46dcf0ae5c9f6a3545
Maxim Usikov <mu.usikov@gmail.com>
2019-04-29 14:32:53 +0300

    feat(utils): add `simplePagination`

---
+++ src/utils/simplePagination.js
@@ -0,0 +1,26 @@
+function simplePagination(currentPage, pageCount) {
+  const delta = 2;
+  const range = [];
+
+  for (
+    let i = Math.max(2, currentPage - delta);
+    i <= Math.min(pageCount - 1, currentPage + delta);
+    i += 1
+  ) {
+    range.push(i);
+  }
+
+  if (currentPage - delta > 2) {
+    range.unshift('...');
+  }
+  if (currentPage + delta < pageCount - 1) {
+    range.push('...');
+  }
+
+  range.unshift(1);
+  range.push(pageCount);
+
+  return range;
+}
+
+export default simplePagination;
