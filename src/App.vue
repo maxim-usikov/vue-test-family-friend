b40a5cb1fecbcb477e470e5fa64a5c781481b963
Maxim Usikov <mu.usikov@gmail.com>
2019-04-29 14:39:20 +0300

    feat: implement task

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,52 @@
 <template>
   <div id="app">
-    TODO: TASK/ff-frontend-test.docx
+    <p style="white-space: pre-wrap">
+      <b>TODO:</b> TASK/ff-frontend-test.docx
+      * [x] search form
+      * [x] history
+      * [x] results
+        * [x] as tabs
+        * [x] as tables
+      * [x] filter
+      * [x] sort by name
+      * [x] sort by stars
+      * [ ] modal for preview repo and author info
+    </p>
+    <SearchForm @search="handleSearch" />
+    <br>
+    <SearchHistory :items="history" />
+    <br>
+    <SearchResults :items="history" />
   </div>
 </template>
 
 <script>
+import searchRepositories from '@/services/api.github/searchRepositories';
+import SearchForm from '@/components/SearchForm.vue';
+import SearchHistory from '@/components/SearchHistory.vue';
+import SearchResults from '@/components/SearchResults.vue';
+
 export default {
   name: 'App',
+
+  components: {
+    SearchForm,
+    SearchHistory,
+    SearchResults,
+  },
+
+  data() {
+    return {
+      history: [],
+    };
+  },
+
+  methods: {
+    async handleSearch(payload) {
+      const response = await searchRepositories(payload.query);
+      this.history.push({ ...payload, response });
+    },
+  },
 };
 </script>
 
@@ -16,5 +56,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  margin: 10px;
 }
 </style>
